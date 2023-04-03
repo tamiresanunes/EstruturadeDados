b1df7ef8307207a53b54d3be659700493c1b6ff9
Tamires Nunes <tamiresnunes.alumo@unipampa.edu.br>
2023-04-03 14:44:38 -0300

    Exercício 2

---
+++ Aluno.java
@@ -0,0 +1,38 @@
+public class Aluno{
+    private String nome;
+    private double nota;
+    private int idade;
+
+    public Aluno(String nome, double nota, int idade){
+        this.nome = nome;
+        this.nota = nota;
+        this.idade = idade;
+    }
+
+    public String getnome(){
+        return nome;
+    }
+    
+    public void setnome(String nome){
+        this.nome = nome;
+    }
+
+    public double getnota(){
+        return nota;
+    }
+
+    public void setnota(double nota) {
+        this.nota = nota;
+        
+    }
+
+    public int getidade(){
+        return idade;
+    }
+
+    public void setidade(int idade){
+        this.idade = idade;
+    }
+
+
+}
--- main.java
+++ main.java
@@ -1,21 +1,18 @@
-public class Main {
-	public static void main(String[] args) {
-		Scanner sc= new Scanner(System.in);
-        Ponto pontoXab= new Ponto("2","2");
-        pontoXab.distanciaAB();
-        Ponto pontoYab = new Ponto("3","3");
-        pontoYab.distanciaAB();
+public class Main{
+    public static void main(String args[]){
+        VetorAluno vet = new VetorAluno();
+        Aluno AL1 = new Aluno();
+        AL1.setnome("Carlos Almeida");
+        vet.adicionar(AL1);
+        if(vet.contem(AL1)){
+            
+        }
+        Nota N1 = new Nota();
+        N1.setnota(9.4);
+        vet.adicionarN(N1);
 
-
-        /*
-        Scanner sc= new Scanner(System.in);
-        System.out.println("Digite o valor do ponto Xa");
-        int Xa = sc.nextInt();
-        System.out.println("Digite o valor do ponto Xb");
-        int Xb = sc.nextInt();
-        System.out.println("Digite o valor do ponto Ya");
-        int Ya = sc.nextInt();
-        System.out.println("Digite o valor do ponto Yb");*/
-	}
+        Idade I1 = new Idade();
+        I1.setidade(20);
+        vet.adicionarI(I1);
+    }
 }
-
